9d72a7dc1e5dcfd26c2cc24c38ee92a870a016a7
Naum Yashuvaev <hahalol915@gmail.com>
2020-10-05 19:54:28 +0300

    almost done

--- project/src/utils.c
+++ project/src/utils.c
@@ -5,182 +5,210 @@
 #include "utils.h"
 
 void cases(int choice) {
-    FILE *client_book = NULL;
-    data client_data = {0};
-    switch (choice) {
-            case 1:
-                add_to_base(client_book, &client_data);
-                break;
-            case 2:
-                read_base(client_book);
-                break;
-            case 3:
-                break;
-            default:
-                puts("error");
-                break;
-    }
+	FILE *client_book = NULL;
+	data client_data = {0};
+	switch (choice) {
+			case 1:
+				add_to_base(client_book, &client_data);
+				break;
+			case 2:
+				read_base(client_book);
+				break;
+			case 3:
+				break;
+			default:
+				puts("error");
+				break;
+	}
 }
 void add_to_base(FILE *client_book, data *client) {
-    client_book = fopen("dataset/database.dat", "a+");
-    if (client_book == NULL) {
-        puts("Not acess");
-        return;
-    }
-    if (scaning(client) == 0) {
-        write_to_file(client_book, client);
-    }
-    fclose(client_book);
+	client_book = fopen("dataset/database.dat", "a+");
+	if (client_book == NULL) {
+		puts("Not acess");
+		return;
+	}
+	if (scaning(client) == 0) {
+		write_to_file(client_book, client);
+	} else {
+		puts("Shit");
+		fclose(client_book);
+		return;
+	}
+	fclose(client_book);
 }
 
 void read_base(FILE *client_book) {
-    client_book = fopen("dataset/database.dat", "r");
+	client_book = fopen("dataset/database.dat", "r");
 
-     if (client_book == NULL) {
-        puts("Not acess");
-        return;
-    }
+	 if (client_book == NULL) {
+		puts("Not acess");
+		return;
+	}
 
-    size_t size = 0;
-    while (!feof(client_book))
-    {
-        if (fgetc(client_book) == '\n')
-            size++;
-    }
-    if (size == 0) {
-    	puts("Base is empty");
-    	fclose(client_book);
-    	return;
-    }
-    size++;
-    //rewind(client_book);
-    fclose(client_book);
- 
-    char **result = NULL;
-    result = alloc_for_array(result, size);
-    if (result == NULL) {
-    	puts("Not acess");
-    	fclose(client_book);
-    	return;
-    }
-    result = write_to_memory(client_book, result);
-    
-    char **sort = NULL;
-    sort = alloc_for_array(sort, size);
-    if (sort == NULL) {
-        puts("Not acess");
-        fclose(client_book);
-        return;
-    }
-    sort = write_to_memory(client_book, sort);
+	size_t size = 0;
+	while (!feof(client_book))
+	{
+		if (fgetc(client_book) == '\n')
+			size++;
+	}
+	if (size == 0) {
+		puts("Base is empty");
+		fclose(client_book);
+		return;
+	}
+	//size++;
+	//rewind(client_book);
+	
+	//rewind(client_book);
+	fclose(client_book);
+	char **result = NULL;
+	result = alloc_for_array(result, size);
+	if (result == NULL) {
+		puts("Not acess");
+		fclose(client_book);
+		return;
+	}
+	result = write_to_memory(client_book, result);
+	//size_t i = 0;
+	
 
-    int buf1[size];
+	/*while (!feof(client_book)) {
+		if (fgets(result[i], 100, client_book) != NULL) {
+			i++;
+		}
+	}*/
+	//size = i;
+	
+	char **sort = NULL;
+	sort = alloc_for_array(sort, size);
+	if (sort == NULL) {
+		puts("Not acess");
+		fclose(client_book);
+		return;
+	}
+	sort = write_to_memory(client_book, sort);
+	//rewind(client_book);
+	//i = 0;
+  
+	/*while (!feof(client_book)) {
+		if (fgets(sort[i], 100, client_book) != NULL){
+			i++;
+		}
+	}*/
 
-    for (size_t i = 0; i < size; ++i) {
-    	char* str = sort[i];
-      	buf1[i] = atoi(strtok(str, " "));
-    }
-    
-    for (size_t i = 0; i < size - 1; ++i) {
-      	for (size_t j = 1 + i; j < size; ++j) {
-            if (buf1[i] > buf1[j]) {
-                char *str = NULL;
-                str = result[i];
-                result[i] = result[j];
-                result[j] = str;
-            }
-      	}
-    }
-    printf("%s\n", "There is your visitors book: ");
-    for (size_t i = 0; i < size; ++i) {
-    	printf("%s\n", result[i]);
-    }
+	//fclose(client_book);
+	
+	int buf1[size];
 
-    free_alloc(result, size);
-    free_alloc(sort, size);
+	for (size_t i = 0; i < size; ++i) {
+		char* str = sort[i];
+		buf1[i] = atoi(strtok(str, " "));
+	}
+	
+	for (size_t i = 0; i < size ; ++i) {
+		for (size_t j = 1 + i; j < size; ++j) {
+			if (buf1[i] > buf1[j]) {
+                int dig = buf1[j];
+                buf1[j] = buf1[i];
+                buf1[i] = dig;
+                char *str;
+                str = result[j];
+                result[j] = result[i];
+                result[i] = str;
+			}
+		}
+	}
+	printf("%s\n", "There is your visitors book: ");
+	for (size_t i = 0; i < size; ++i) {
+		printf("%s\n", result[i]);
+	}
+
+	free_alloc(result, size);
+	free_alloc(sort, size);
 }
 
 void copy_menu() {
-    printf("please enter action\n"
-           "1 add client;\n"
-           "2 show data client;\n"
-           "3 exit;\n");
+	printf("\nPlease enter action:\n"
+		   "1 Add client;\n"
+		   "2 Show data client;\n"
+		   "3 Exit;\n");
 }
 
 int scaning(data *client) {
-    while (scanf("%5d%20s%20s%12lf",
-           &client->table_number,
-           client->name,
-           client->surname,
-           &client->payments) != -1) {
-        return 0;
-    }
-    return 1;
+	while (scanf("%5d%20s%20s%12lf",
+		   &client->table_number,
+		   client->name,
+		   client->surname,
+		   &client->payments) != -1) {
+		return 0;
+	}
+	return 1;
 }
 int write_to_file(FILE *visitors_book, data *got_data) {
-    int check;
-    check = fprintf(visitors_book, "\n%-12d%-15s%-15s%12.2f",
-                         got_data->table_number,
-                         got_data->name,
-                         got_data->surname,
-                         got_data->payments);
-        
-    return check;
+	int check;
+	check = fprintf(visitors_book, "%-12d%-15s%-15s%12.2f\n",
+						 got_data->table_number,
+						 got_data->name,
+						 got_data->surname,
+						 got_data->payments);
+		
+	return check;
 }
 
 int free_alloc(char** paragraph, size_t rows) {
-    if (paragraph == NULL) {
-    	puts("fuck");
-        return -1;
-    }
-    for (size_t i = 0; i < rows; i++) {
-        free(paragraph[i]);
-    }
-    free(paragraph);
-    return 0;
+	if (paragraph == NULL) {
+		puts("fuck");
+		return -1;
+	}
+	for (size_t i = 0; i < rows; i++) {
+		free(paragraph[i]);
+	}
+	free(paragraph);
+	return 0;
 }
 
 
 char** alloc_for_array(char **paragraph, size_t size) {
-    paragraph = (char **)malloc(size*sizeof(char *));
+	paragraph = (char **)malloc(size*sizeof(char *));
 
-    if (paragraph == NULL) {
-        return NULL;
-    }
+	if (paragraph == NULL) {
+		return NULL;
+	}
 
-    for (size_t i = 0; i < size; i++) {
-        paragraph[i] = (char *)malloc(100*sizeof(char));
-        if (paragraph[i] == NULL) {
-	        for (size_t a = 0; a < i; a++) {
-                free(paragraph[a]);
-            }
-	        free(paragraph);
-            return NULL;
-        }
-    }
-    return paragraph;
+	for (size_t i = 0; i < size; i++) {
+		paragraph[i] = (char *)malloc(100*sizeof(char));
+		if (paragraph[i] == NULL) {
+			for (size_t a = 0; a < i; a++) {
+				free(paragraph[a]);
+			}
+			free(paragraph);
+			return NULL;
+		}
+	}
+	return paragraph;
 }
 
 char** write_to_memory(FILE *client_book, char **paragraph) {
 	client_book = fopen("dataset/database.dat", "r");
 
-     if (client_book == NULL) {
-        puts("Not acess");
-        return NULL;
-    }
+	 if (client_book == NULL) {
+		puts("Not acess");
+		return NULL;
+	}
+	rewind(client_book);
 
-    if (paragraph == NULL) {
-    	fclose(client_book);
-    	return NULL;
-    }
-    size_t i = 0;
-    while (!feof(client_book)) {
-        if (fgets(paragraph[i], 100, client_book) != NULL){
-            i++;
-   	    }
-    }
-    rewind(client_book);
-    fclose(client_book);
-    return paragraph;
+	if (paragraph == NULL) {
+		fclose(client_book);
+		return NULL;
+	}
+	size_t i = 0;
+	while (!feof(client_book)) {
+		if (fgets(paragraph[i], 100, client_book) != NULL){
+			i++;
+		}
+	}
+	
+	fclose(client_book);
+	return paragraph;
 
 }
